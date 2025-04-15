eb1a9e836942166e59cd5a519f4b91e1a05281c9
annatarusova <annabelemenko1@gmail.com>
2025-04-16 01:30:47 +0300

    Задание 11й спринт, fix codestyle

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -6,9 +6,14 @@ import ru.yandex.practicum.filmorate.model.Film;
 import java.util.List;
 
 public interface FilmStorage {
+
     Film create(@RequestBody Film film);
+
     Film update(@RequestBody Film film);
+
     void delete(long id);
+
     Film getFilm(long id);
+
     List<Film> getFilms();
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
@@ -7,10 +7,15 @@ import ru.yandex.practicum.filmorate.model.User;
 import java.util.List;
 
 public interface UserStorage {
+
     User create(@RequestBody User user);
+
     User update(@RequestBody User user);
+
     void delete(long id);
+
     User getUser(long id);
+
     void ensureUserExists(long id) throws NotFoundException;
 
     List<User> getUsers();
